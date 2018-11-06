54f4eecb85c0f276ec3a37e369966613356d3a57
skothari-tibco <skothari@tibco.com>
2018-11-06 15:19:19 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,98 @@
+<!--
+title: CSV Timer
+
+-->
+# Timer Trigger
+This trigger provides your flogo application the ability to get data from a CSV file after a particular interval for an action 
+
+## Installation
+
+```bash
+flogo install github.com/skothari-tibco/csvtrigger
+```
+
+## Metadata
+```json
+{
+  "handler": {
+    "settings": [
+      {
+        "name": "filePath",
+        "type": "string"
+      },
+      {
+        "name": "repeatInterval",
+        "type": "string"
+      }
+    ]
+  }
+}
+```
+### Details
+
+#### Handler Settings:
+| Setting  | Required | Description |
+|:---------|:---------|:------------|
+| filePath   | true     | Path to a CSV file
+| repeatInterval | false     | the repeat interval (1, 200 etc in second), doesn't repeat if not specified
+
+
+## Example Configurations
+
+Triggers are configured via the triggers.json of your application. The following are some example configuration of the Timer Trigger.
+
+
+### Only once immediatley
+Configure the Trigger to run a flow once with a delay of one minute.  "startDelay" settings format = "[hours]h[minutes]m[seconds]s"
+
+```json
+{
+  "triggers": [
+    {
+      "id": "flogo-timer",
+      "ref": "github.com/project-flogo/contrib/trigger/timer",
+      "handlers": [
+        {
+          "settings": {
+            "filePath": "path_to_file"
+          },
+          "action": {
+            "ref": "github.com/project-flogo/flow",
+            "settings": {
+              "flowURI": "res://flow:myflow"
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
+```
+
+### Repeating
+Configure the Trigger to run a flow repeating every 10 minutes. "repeatInterval" settings format = "[hours]h[minutes]m[seconds]s"
+
+```json
+{
+  "triggers": [
+    {
+      "id": "flogo-timer",
+      "ref": "github.com/project-flogo/contrib/trigger/timer",
+      "handlers": [
+        {
+          "settings": {
+	    "filePath": "path_to_file",
+            "repeatInterval": "10"
+          },
+          "action": {
+            "ref": "github.com/project-flogo/flow",
+            "settings": {
+              "flowURI": "res://flow:myflow"
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
+```
---
+++ timer.go
@@ -0,0 +1,249 @@
+package csvtimer
+
+import (
+	"context"
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/carlescere/scheduler"
+	"github.com/project-flogo/core/data/metadata"
+	"github.com/project-flogo/core/support/log"
+	"github.com/project-flogo/core/trigger"
+)
+
+var COUNT int
+var LINES [][]string
+
+type HandlerSettings struct {
+	StartInterval  string `md:"startDelay"`
+	RepeatInterval string `md:"repeatInterval"`
+	FilePath       string `md:"filePath"`
+}
+
+type Output struct {
+	Data  interface{} `md:"data"`
+	Error interface{} `md:"error"`
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Data":  o.Data,
+		"Error": o.Error,
+	}
+}
+
+var triggerMd = trigger.NewMetadata(&HandlerSettings{}, &Output{})
+
+func init() {
+	trigger.Register(&Trigger{}, &Factory{})
+}
+
+type Factory struct {
+}
+
+// Metadata implements trigger.Factory.Metadata
+func (*Factory) Metadata() *trigger.Metadata {
+	return triggerMd
+}
+
+// New implements trigger.Factory.New
+func (*Factory) New(config *trigger.Config) (trigger.Trigger, error) {
+	return &Trigger{}, nil
+}
+
+type Trigger struct {
+	timers   []*scheduler.Job
+	handlers []trigger.Handler
+	logger   log.Logger
+}
+
+// Init implements trigger.Init
+func (t *Trigger) Initialize(ctx trigger.InitContext) error {
+	COUNT = 0
+	t.handlers = ctx.GetHandlers()
+	t.logger = ctx.Logger()
+
+	return nil
+}
+
+// Start implements ext.Trigger.Start
+func (t *Trigger) Start() error {
+
+	handlers := t.handlers
+
+	for _, handler := range handlers {
+
+		s := &HandlerSettings{}
+		err := metadata.MapToStruct(handler.Settings(), s, true)
+		if err != nil {
+			return err
+		}
+
+		if s.RepeatInterval == "" {
+			t.scheduleOnce(handler, s)
+		} else {
+			t.scheduleRepeating(handler, s)
+		}
+	}
+
+	return nil
+}
+
+// Stop implements ext.Trigger.Stop
+func (t *Trigger) Stop() error {
+
+	for _, timer := range t.timers {
+
+		if timer.IsRunning() {
+			timer.Quit <- true
+		}
+	}
+
+	t.timers = nil
+
+	return nil
+}
+
+func (t *Trigger) scheduleOnce(handler trigger.Handler, settings *HandlerSettings) error {
+
+	seconds := 0
+
+	if settings.StartInterval != "" {
+		d, err := time.ParseDuration(settings.StartInterval)
+		if err != nil {
+			return fmt.Errorf("unable to parse start delay: %s", err.Error())
+		}
+
+		seconds = int(d.Seconds())
+		t.logger.Debugf("Scheduling action to run once in %d seconds", seconds)
+	}
+
+	timerJob := scheduler.Every(seconds).Seconds()
+
+	fn := func() {
+		t.logger.Debug("Executing \"Once\" timer trigger")
+
+		data, err := ReadCsv(settings.FilePath)
+
+		triggerData := make(map[string]interface{})
+
+		triggerData["data"] = data
+
+		triggerData["error"] = err
+
+		_, err = handler.Handle(context.Background(), triggerData)
+		if err != nil {
+			t.logger.Error("Error running handler: ", err.Error())
+		}
+
+		if timerJob != nil {
+			timerJob.Quit <- true
+		}
+	}
+
+	if seconds == 0 {
+		t.logger.Debug("Start delay not specified, executing action immediately")
+		fn()
+	} else {
+		timerJob, err := timerJob.NotImmediately().Run(fn)
+		if err != nil {
+			t.logger.Error("Error scheduling execute \"once\" timer: ", err.Error())
+		}
+
+		t.timers = append(t.timers, timerJob)
+	}
+
+	return nil
+}
+
+func (t *Trigger) scheduleRepeating(handler trigger.Handler, settings *HandlerSettings) error {
+	t.logger.Info("Scheduling a repeating timer")
+	t.logger.Info("reapeat")
+	startSeconds := 0
+
+	repeatInterval, _ := strconv.Atoi(settings.RepeatInterval)
+	t.logger.Info("reapeat", repeatInterval)
+	t.logger.Debugf("Scheduling action to repeat every %d seconds", repeatInterval)
+
+	fn := func() {
+		t.logger.Debug("Executing \"Repeating\" timer")
+
+		triggerData := make(map[string]interface{})
+		data, err := ReadCsvInterval(settings.FilePath)
+		if err != nil {
+			return
+		}
+		triggerData["data"] = data
+
+		triggerData["error"] = ""
+
+		fmt.Println("Starting flow")
+		_, err = handler.Handle(context.Background(), triggerData)
+		COUNT = COUNT + 1
+		if err != nil {
+			t.logger.Error("Error running handler: ", err.Error())
+		}
+	}
+
+	if startSeconds == 0 {
+
+		timerJob, err := scheduler.Every(repeatInterval).Seconds().Run(fn)
+		if err != nil {
+			t.logger.Error("Error scheduling repeating timer: ", err.Error())
+		}
+
+		t.timers = append(t.timers, timerJob)
+	}
+
+	return nil
+}
+
+//type PrintJob struct {
+//	Msg string
+//}
+//
+//func (j *PrintJob) Run() error {
+//	t.logger.Debug(j.Msg)
+//	return nil
+//}
+
+func ReadCsv(path string) (interface{}, error) {
+
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+
+	lines, err := csv.NewReader(f).ReadAll()
+
+	if err != nil {
+		return "", err
+	}
+
+	return lines, nil
+}
+
+func ReadCsvInterval(path string) (interface{}, error) {
+	if COUNT == 0 {
+		data, err := ReadCsv(path)
+		if err != nil {
+			return nil, err
+		}
+		LINES = data.([][]string)
+
+		COUNT = COUNT + 1
+
+		return LINES[0], nil
+	}
+	if COUNT == len(LINES) {
+		return nil, errors.New("Done")
+	}
+	return LINES[COUNT], nil
+
+}
